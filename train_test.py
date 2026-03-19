60f940a800a0f88b319072ebd8415ce89562e622
anavpineda <pineda_a1@denison.edu>
2026-03-19 18:45:40 -0500

    Create train_test.py

---
+++ train_test.py
@@ -0,0 +1,17 @@
+from cellpose import io, models, train
+io.logger_setup()
+
+train_dir = "./Slices/train/"
+test_dir  = "./Slices/test/"
+
+output = io.load_train_test_data(train_dir, test_dir, image_filter="_img",
+                                mask_filter="_masks", look_one_level_down=False)
+images, labels, image_names, test_images, test_labels, image_names_test = output
+
+model = models.CellposeModel(gpu=True)
+
+model_path, train_losses, test_losses = train.train_seg(model.net,
+                            train_data=images, train_labels=labels,
+                            test_data=test_images, test_labels=test_labels,
+                            weight_decay=0.1, learning_rate=1e-5,
+                            n_epochs=100, model_name="neuromast_cellpose_trained_model")
