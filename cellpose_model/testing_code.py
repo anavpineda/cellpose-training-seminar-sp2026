f998f26682c69df8e796f4ea8e33608da959714b
Ana Pineda <apineda@login-02.clusternet>
2026-03-23 14:26:45 -0700

    final test code

--- cellpose_model/testing_code.py
+++ cellpose_model/testing_code.py
@@ -7,10 +7,10 @@ from skimage import io as skio
 import os
 io.logger_setup()
 
-nuclei_path = "/hpc/projects/jacobo_group/iSim_processed_files/HCR/2025-06-25_N2V_model_training_tif/final_hcr_results/dld/400ly/denoised/tiffs/fish_400lyadldfish3/she_3.tiff"
-membrane_path = "/hpc/projects/jacobo_group/iSim_processed_files/HCR/2025-06-25_N2V_model_training_tif/final_hcr_results/dld/400ly/denoised/tiffs/fish_400lyadldfish3/membrane_3.tiff"
+nuclei_path = "/hpc/projects/jacobo_group/iSim_processed_files/HCR/2025-06-25_N2V_model_training_tif/final_hcr_results/dla/dmso/denoised/tiffs/fish_dmsobdlafish1/she_2.tiff"
+membrane_path = "/hpc/projects/jacobo_group/iSim_processed_files/HCR/2025-06-25_N2V_model_training_tif/final_hcr_results/dla/dmso/denoised/tiffs/fish_dmsobdlafish1/membrane_2.tiff"
 model_path = "/hpc/projects/jacobo_group/projects/cellpose/Membranes/2_Channel/HCR/Fine_Tune/models/neuromast_cellpose_trained_model"
-output_path = "/hpc/projects/jacobo_group/projects/cellpose/Membranes/2_Channel/HCR/git/cellpose-training-seminar-sp2026/cellpose_model"
+output_path = "/hpc/projects/jacobo_group/projects/cellpose/Membranes/2_Channel/HCR/Fine_Tune/final_outputs"
 
 nuclei = imread(nuclei_path)
 membrane = imread(membrane_path)
@@ -22,5 +22,5 @@ img[1,:,:,:] = membrane
 model = models.CellposeModel(pretrained_model=model_path, gpu=True)
 masks, flows, styles = model.eval(img, diameter=None, channels=[1,2], stitch_threshold=0.5, channel_axis=0, z_axis=1)
 
-save_name = os.path.join(output_path, "400fish3dld.tiff")
+save_name = os.path.join(output_path, "dmsodlafish1_masks.tiff")
 skio.imsave(save_name, masks, check_contrast=False)
